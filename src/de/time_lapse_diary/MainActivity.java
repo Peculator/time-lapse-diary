409888b4fb91fac6ef9494086a7637aa6cd2a929
Peculator <sven_86@gmx.net>
2014-02-05 23:30:15 +0100

    GridView + first Item

--- src/de/time_lapse_diary/MainActivity.java
+++ src/de/time_lapse_diary/MainActivity.java
@@ -5,6 +5,11 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +19,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		 GridView gridview = (GridView) findViewById(R.id.gridview);
+		    gridview.setAdapter(new ImageAdapter(this));
+		    		
+		    	OnItemClickListener v =	new AdapterView.OnItemClickListener() {
+		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		            Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+		        }
+		    };
+		    gridview.setOnItemClickListener(v);
 	}
 
 	@Override
