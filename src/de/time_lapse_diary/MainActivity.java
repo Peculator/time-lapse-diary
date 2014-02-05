4addce2de364c5b305ff4eb2edc3917bbba435c4
Peculator <sven_86@gmx.net>
2014-02-06 00:44:16 +0100

    GridViewItems + Backgroundcolor

--- src/de/time_lapse_diary/MainActivity.java
+++ src/de/time_lapse_diary/MainActivity.java
@@ -1,5 +1,7 @@
 package de.time_lapse_diary;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -13,42 +15,69 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public static String tag = "tld";
+	public static String tag = "tld";
+	private GridviewAdapter mAdapter;
+    private ArrayList<String> listNames;
+    private ArrayList<Integer> listIDs;
+    
+    private GridView gridView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		 GridView gridview = (GridView) findViewById(R.id.gridview);
-		    gridview.setAdapter(new ImageAdapter(this));
-		    		
-		    	OnItemClickListener v =	new AdapterView.OnItemClickListener() {
-		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-		            Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-		        }
-		    };
-		    gridview.setOnItemClickListener(v);
+		 // prepared arraylist and passed it to the Adapter class
+		prepareList();
+		
+        mAdapter = new GridviewAdapter(this,listNames, listIDs);
+ 
+        // Set custom adapter to gridview
+        gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(mAdapter);
+ 
+        // Implement On Item click listener
+        gridView.setOnItemClickListener(new OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                    long arg3) {
+                Toast.makeText(MainActivity.this, mAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 	}
+	
+	public void prepareList()
+    {
+          listNames = new ArrayList<String>();
+ 
+          listNames.add("india");
+          listNames.add("Brazil");
+          
+          listIDs = new ArrayList<Integer>();
+          listIDs.add(R.drawable.ic_launcher);
+          listIDs.add(R.drawable.ic_launcher);
+          
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // settings clicked
-                break;
-            default:
-                Log.w(tag,"Unknown Option selected");
-        }
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			// settings clicked
+			break;
+		default:
+			Log.w(tag, "Unknown Option selected");
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
